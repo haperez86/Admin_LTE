322a8513fac9fba52b49ac538f631d82b9ec6781
henry perez <haperez86@gmail.com>
2020-06-03 12:18:25 -0500

    se realizan cambios en el repositorio

--- vistas/modulos/usuarios.php
+++ vistas/modulos/usuarios.php
@@ -34,7 +34,7 @@
 
       <div class="box-body">
         
-      <table class="table table-bordered table-striped">
+      <table class="table table-bordered table-striped dt-responsive tablas">
           <thead>
               <tr>
                   <th>#</th>
@@ -91,26 +91,118 @@
 
 <!-- Ventana Modal -->
 
-<!-- Modal -->
-
 <div id="modalAgregarUsuario" class="modal fade" role="dialog">
 
   <div class="modal-dialog">
 
-    <!-- Modal content-->
     <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="close" data-dismiss="modal">&times;</button>
-        <h4 class="modal-title">Modal Header</h4>
-      </div>
 
-      <div class="modal-body">
-        <p>Some text in the modal.</p>
-      </div>
+    <form role="form" method="post" enctype="multipart/form-data">
 
-      <div class="modal-footer">
-        <button type="button" class="btn btn-primary" data-dismiss="modal">Close</button>
-      </div>
+        <div class="modal-header" style="background:#3c8dbc; color:white">
+
+          <button type="button" class="close" data-dismiss="modal">&times;</button>
+
+          <h4 class="modal-title">Agregar usuario</h4>
+
+        </div>
+
+        <div class="modal-body">
+
+          <div class="box-body">
+
+            <!--ENTRADA PARA EL NOMBRE-->
+
+            <div class="form-group">
+
+              <div class="input-group">
+
+                  <span class="input-group-addon"><i class="fa fa-user"></i></span>
+
+                  <input type="text" class="form-control input-lg" name="nuevoNombre" placeholder="Ingresar nombre" required>
+              
+              </div>
+            
+            </div>
+
+            <!--ENTRADA PARA EL USUARIO-->
+
+            <div class="form-group">
+
+              <div class="input-group">
+
+                  <span class="input-group-addon"><i class="fa fa-key"></i></span>
+
+                  <input type="text" class="form-control input-lg" name="nuevoUsuario" placeholder="Ingresar usuario" required>
+
+              </div>
+
+            </div>
+
+            <!--ENTRADA PARA LA CONTRASEÑA-->
+
+            <div class="form-group">
+
+                <div class="input-group">
+
+                    <span class="input-group-addon"><i class="fa fa-lock"></i></span>
+
+                    <input type="text" class="form-control input-lg" name="nuevoPassword" placeholder="Ingresar Contraseña" required>
+
+                </div>
+
+            </div>
+
+            <!--ENTRADA PARA SELECCIONAR SU PERFIL-->
+
+            <div class="form-group">
+
+              <div class="input-group">
+
+                  <span class="input-group-addon"><i class="fa fa-users"></i></span>
+
+                  <select class="form-control input-lg" name="nuevoPerfil">
+                  
+                      <option value="">Selecionar perfil</option>
+
+                      <option value="Administrador">Administrador</option>
+
+                      <option value="Especial">Especial</option>
+
+                      <option value="Vendedor">Vendedor</option>
+                  
+                  </select>
+              </div>
+
+            </div>
+
+            <!--ENTRADA PARA SELECCIONAR FOTO-->
+
+          <div class="form-group">
+
+            <div class="panel"> SUBIR FOTO </div>
+
+            <input type="file" id="nuevaFoto" name="nuevaFoto">
+
+                <p class="help-block">Peso máximo de la foto 200 MB</p>
+
+                <img src="vistas/img/usuarios/default/anonymous.png" alt="img-thumbnail" width="100px">    
+
+          </div>
+
+        </div>
+
+        </div>
+
+        <div class="modal-footer">
+
+          <button type="button" class="btn btn-default pull-left" data-dismiss="modal">Salir</button>
+
+          <button type="submit" class="btn btn-primary">Guardar cambios</button>
+        
+        </div>
+      
+      </form>
 
     </div>
 
